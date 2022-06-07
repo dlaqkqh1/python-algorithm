7bf28162192658f248d9a57405d86febe1108a7b
dlaqkqh1 <dlaqkqh1@gmail.com>
2022-06-08 00:04:32 +0900

    python 정적, 클래스 메서드 공부

---
+++ Study/class_test3.py
@@ -0,0 +1,25 @@
+class Time:
+    def __init__(self, hour, minute, second):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+      
+    @staticmethod
+    def is_time_valid(time_string):
+        hour, minute, second = map(int, time_string.split(':'))
+        return hour <= 24 and minute <= 59 and second <= 60
+
+    @classmethod
+    def from_string(cls, time_string):
+        cls.hour, cls.minute, cls.second = map(int, time_string.split(':'))
+        return cls(cls.hour, cls.minute, cls.second)
+
+
+    
+time_string = input()
+ 
+if Time.is_time_valid(time_string):
+    t = Time.from_string(time_string)
+    print(t.hour, t.minute, t.second)
+else:
+    print('잘못된 시간 형식입니다.')
